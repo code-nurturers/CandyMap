ec63d1261a221e1ce3d054bdb449e85b2be642d8
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-10-25 08:29:58 -0500

    pull locations from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import CandyMap from './CandyMap'
+const api_base_url = "https://spooky-api.azurewebsites.net"
 function App() {
-  const candyLocations = [
-    {
-      lat: 29.5522, 
-      long: -95.0973,
-      name: 'You are here'
-    }
-  ]
+  const [candyLocations,setCandyLocations = useState([]); 
   const mapCenter = [29.5522, -95.0973]
+  async function fetchCandyLocations(){
+    try {
+      const data = await fetch(`${api_base_url}/candy-locations`)  
+      const cls = await data.json()
+      setCandyLocations(cls)
+    } catch (error) {
+      console.error(error)
+    }
+    
+  }
+  useEffect(() => {
+    fetchCandyLocations()
+  },[])
   return (
     <div className="App">
       <CandyMap
